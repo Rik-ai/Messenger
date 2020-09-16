6cb3075cefa2c322607bfad5fc61db609c7d9a8d
Ivan <ivan.lakhanin@gmail.com>
2020-09-16 17:35:15 +0300

    Styled sidebar head

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -3,12 +3,13 @@ import styled from './Sidebar.module.css'
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { IconButton } from '@material-ui/core';
+import { IconButton, Avatar } from '@material-ui/core';
 
 const Sidebar = ()=>{
     return(
         <div className={styled.sidebar}>
             <div className={styled.header}>
+                <Avatar src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTGZTjKlvneVt9s8JxDmuaPy1YjmKTxT1FgAg&usqp=CAU'/>
                 <div className={styled.headerRight}>
                     <IconButton>
                      <DonutLargeIcon />
