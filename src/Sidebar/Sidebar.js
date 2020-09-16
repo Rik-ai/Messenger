cb85d4eb308bf9ac9496292c64b634f6d83d58ac
Ivan <ivan.lakhanin@gmail.com>
2020-09-16 17:25:36 +0300

    Add icons

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import styled from './Sidebar.module.css'
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { IconButton } from '@material-ui/core';
 
 const Sidebar = ()=>{
     return(
         <div className={styled.sidebar}>
             <div className={styled.header}>
                 <div className={styled.headerRight}>
-                    <DonutLargeIcon />
+                    <IconButton>
+                     <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                     <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                     <MoreVertIcon />
+                    </IconButton>
                 </div>
             </div>
         </div>
