2457500aa855b0e33ea1350271680ade931652e8
Ivan <ivan.lakhanin@gmail.com>
2020-09-18 18:03:51 +0300

    Add Pusher lib

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from './App.module.css'
 import Sidebar from './Sidebar/Sidebar'
 import Chat from './Chat/Chat'
+import Pusher from 'pusher-js'
 
 function App() {
+
+  useEffect(() => {
+  const pusher = new Pusher('2cff151ee712296fed25', {
+    cluster: 'eu'
+  });
+
+  const channel = pusher.subscribe('messages');
+  channel.bind('inserted', (data) => {
+    alert(JSON.stringify(data));
+  });
+    
+  }, [])
+  
   return (
     <div className={styled.app}>
       <div className={styled.body}>
