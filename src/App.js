4c3ed4bd15f0128969dfb7a325fcec3893ea454f
Ivan <ivan.lakhanin@gmail.com>
2020-09-20 21:24:19 +0300

    add Axios and  messages in console

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from './App.module.css'
 import Sidebar from './Sidebar/Sidebar'
 import Chat from './Chat/Chat'
 import Pusher from 'pusher-js'
+import axios from'./axios'
 
 function App() {
+  const [messages, setMessages] = useState([])
+
+useEffect(() => {
+  axios.get('/messages/sync')
+    .then(response=>{
+      setMessages(response.data)
+    })
+}, [])
+
 
   useEffect(() => {
-  const pusher = new Pusher('2cff151ee712296fed25', {
-    cluster: 'eu'
+    const pusher = new Pusher('2cff151ee712296fed25', {
+      cluster: 'eu'
   });
 
   const channel = pusher.subscribe('messages');
-  channel.bind('inserted', (data) => {
-    alert(JSON.stringify(data));
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage])
   });
-    
-  }, [])
+
+    return ()=>{
+      channel.unbind_all()
+      channel.unsubscribe()
+    }
+  }, [messages])
+
+  console.log(messages)
+
   
   return (
     <div className={styled.app}>
